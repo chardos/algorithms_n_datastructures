d2d7ae074d66b43ff958847392d2e27c9800e303
Richard Tan <chardos@gmail.com>
2018-05-28 21:12:43 +1000

    Binary search

--- src/algorithms/BinarySearch.js
+++ src/algorithms/BinarySearch.js
@@ -1,1 +1,16 @@
 // binary search
+const binarySearch = (arr, target, lowerBound=0, upperBound=arr.length-1) => {
+    const middle = Math.round((upperBound + lowerBound) / 2);
+    const middleValue = arr[middle];
+
+    if (middleValue === target) {
+        return middle;
+    } else if (middleValue > target) {
+        return binarySearch(arr, target, lowerBound, middle - 1);
+    } else if (middleValue < target) {
+        return binarySearch(arr, target, middle + 1, upperBound);
+    }
+}
+
+const arr = [2,5,12,33,34,35,54,65,76,88,98]
+const index = binarySearch(arr, 88);
